faff04a2ff91cf762a563f8737d496f454a7a888
sirsemy <49652813+sirsemy@users.noreply.github.com>
2025-01-07 13:54:19 +0100

    Update Horse-racing Duals.js with working code

--- Horse-racing Duals.js
+++ Horse-racing Duals.js
@@ -3,12 +3,27 @@
  * the standard input according to the problem statement.
  **/
 
-const N = parseInt(readline());
-for (let i = 0; i < N; i++) {
-    const pi = parseInt(readline());
+const horseAmount = parseInt(readline());
+
+let horseStrenghts = [];
+
+for (let i = 0; i < horseAmount; i++) {
+    horseStrenghts[i] = parseInt(readline());
+}
+
+let sortedStrenghts = horseStrenghts.toSorted((a, b) => a - b);
+
+let closestDifference = 10000000;
+
+for (let i = 1; i < horseAmount; i++) {
+    let difference = sortedStrenghts[i] - sortedStrenghts[i - 1];
+
+    if (difference < closestDifference) {
+        closestDifference = difference;
+    }
 }
 
 // Write an answer using console.log()
 // To debug: console.error('Debug messages...');
 
-console.log('answer');
+console.log(closestDifference);
