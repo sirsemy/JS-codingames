cb1848f252d1f4989327d63ea2a146d8ec5f61ad
sirsemy <49652813+sirsemy@users.noreply.github.com>
2025-01-07 13:14:31 +0100

    Add working code to MIME TYPE game

--- MIME  TYPE.js
+++ MIME  TYPE.js
@@ -16,5 +16,34 @@ for (let i = 0; i < rowToBeProcessed; i++) {
     mimeType[i] = inputs[1]; // MIME type.
 }
 
+function indentifyFileMimeType(originalFileName)
+{
+    let fileNameParts = originalFileName.split('.');
+
+    if (fileNameParts.length < filePartNumber) {
+        return "UNKNOWN";
+    }
+
+    let lastFileNamePart = fileNameParts.pop().toLowerCase();
+    
+    let whichIndex = extensions.findIndex(
+        (element) => element === lastFileNamePart
+    );
+
+    if (whichIndex == -1) {
+        return "UNKNOWN";
+    }
+
+    return mimeType[whichIndex];
+}
+
+for (let i = 0; i < fileNameAmount; i++) {
+    const fileName = readline(); // One file name per line.
+
+    let display = indentifyFileMimeType(fileName);
+
+    console.log(display);
+}
+
 // Write an answer using console.log()
 // To debug: console.error('Debug messages...');
