e19c9c7dc9b1fb67a3fd45fdf2fc35fe279d7a63
sirsemy <49652813+sirsemy@users.noreply.github.com>
2025-01-07 13:18:23 +0100

    Create Horse-racing Duals.js

---
+++ Horse-racing Duals.js
@@ -0,0 +1,14 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N = parseInt(readline());
+for (let i = 0; i < N; i++) {
+    const pi = parseInt(readline());
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
